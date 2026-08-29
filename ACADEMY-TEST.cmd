b05c035883b32e9f817e58c76a4ff4ac6ddb7b91
communauticneu <office@communautic.com>
2026-08-29 09:15:26 +0200

    feat: write sanitized local test diagnostics

--- ACADEMY-TEST.cmd
+++ ACADEMY-TEST.cmd
@@ -2,6 +2,9 @@
 setlocal
 cd /d "%~dp0"
 title Webbased Academy Creator - Teststart
+set "DIAG_FILE=academy-diagnostics.txt"
+set "TEST_STATUS=STARTED"
+call :write_diagnostics
 
 echo.
 echo ==============================================
@@ -19,6 +22,8 @@ echo.
 echo [2/3] Vollstaendige Testsuite ausfuehren...
 call npm.cmd test
 if errorlevel 1 goto :error
+set "TEST_STATUS=GREEN"
+call :write_diagnostics
 
 echo.
 echo [3/3] Alle Tests gruen - Creator wird gestartet...
@@ -27,15 +32,27 @@ call npm.cmd start
 goto :end
 
 :error
+set "TEST_STATUS=ERROR"
+call :write_diagnostics
 echo.
 echo ==============================================
 echo   FEHLER - Creator wurde NICHT gestartet.
 echo ==============================================
 echo.
-echo Bitte dieses Fenster offen lassen und einen Screenshot senden.
+echo Diagnose wurde lokal in academy-diagnostics.txt gespeichert.
 echo.
 pause
 exit /b 1
+
+:write_diagnostics
+>"%DIAG_FILE%" echo Webbased Academy Creator - Diagnose
+>>"%DIAG_FILE%" echo Zeitpunkt: %DATE% %TIME%
+>>"%DIAG_FILE%" echo Teststatus: %TEST_STATUS%
+>>"%DIAG_FILE%" <nul set /p "=Version: "
+>>"%DIAG_FILE%" node -p "require('./package.json').version" 2>nul
+>>"%DIAG_FILE%" <nul set /p "=Commit: "
+>>"%DIAG_FILE%" git rev-parse --short HEAD 2>nul
+exit /b 0
 
 :end
 endlocal
