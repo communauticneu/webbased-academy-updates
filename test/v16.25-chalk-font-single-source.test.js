5b7582c8f3604eb7593395c687580eea0d97fc58
communauticneu <office@communautic.com>
2026-08-30 18:09:49 +0200

    test: lock approved KG heading and DJB body font mapping

--- test/v16.25-chalk-font-single-source.test.js
+++ test/v16.25-chalk-font-single-source.test.js
@@ -2,16 +2,14 @@ const test=require('node:test');
 const assert=require('node:assert/strict');
 const fs=require('node:fs');
 const path=require('node:path');
-
 const ux=fs.readFileSync(path.join(__dirname,'../src/presentation-text-direct-ux.js'),'utf8');
 const fonts=fs.readFileSync(path.join(__dirname,'../src/academy-fonts.css'),'utf8');
-const editor=fs.readFileSync(path.join(__dirname,'../src/presentation-object-editor.js'),'utf8');
 
-test('chalkboard heading uses KG and normal plus small use bundled DJB face only',()=>{
-  assert.match(fonts,/font-family:"Academy DJB Chalk";[\s\S]*DJB Chalk It Up\.ttf/);
-  assert.match(ux,/academy-text-heading[^`]*Academy KG Sketch/s);
-  assert.match(ux,/academy-text-normal[^`]*Academy DJB Chalk/s);
-  assert.match(ux,/academy-text-small[^`]*Academy DJB Chalk/s);
-  assert.doesNotMatch(ux,/Segoe Print|Comic Sans MS/);
-  assert.doesNotMatch(editor,/Segoe Print|Comic Sans MS/);
+test('Tafel heading is KG and Normal plus Klein are DJB Chalk It Up',()=>{
+ assert.match(fonts,/Academy KG Sketch[\s\S]*KGSecondChancesSketch\.ttf/);
+ assert.match(fonts,/Academy DJB Chalk[\s\S]*DJB Chalk It Up\.ttf/);
+ assert.match(ux,/academy-text-heading[\s\S]*Academy KG Sketch/);
+ assert.match(ux,/academy-text-normal[\s\S]*Academy DJB Chalk/);
+ assert.match(ux,/academy-text-small[\s\S]*Academy DJB Chalk/);
+ assert.doesNotMatch(ux,/Segoe Print|Comic Sans MS/);
 });
