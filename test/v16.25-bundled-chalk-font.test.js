cf89d9bd07ef864750e4283ac8f194cc7474c523
communauticneu <office@communautic.com>
2026-08-30 13:16:07 +0200

    test: require bundled chalk font asset path

--- test/v16.25-bundled-chalk-font.test.js
+++ test/v16.25-bundled-chalk-font.test.js
@@ -3,18 +3,14 @@ const assert=require('node:assert/strict');
 const fs=require('node:fs');
 const path=require('node:path');
 
-const preload=fs.readFileSync(path.join(__dirname,'../src/preload.js'),'utf8');
+const ux=fs.readFileSync(path.join(__dirname,'../src/presentation-text-direct-ux.js'),'utf8');
 
-test('preload loads the bundled chalk font before direct text UX',()=>{
-  const fontIndex=preload.indexOf('kg-second-chances-sketch-font.js');
-  const uxIndex=preload.indexOf('presentation-text-direct-ux.js');
-  assert.ok(fontIndex>=0,'bundled chalk font loader must be present');
-  assert.ok(uxIndex>=0 && fontIndex<uxIndex,'font loader must run before direct text UX');
+test('chalkboard typography declares a bundled KG Second Chances Sketch font face',()=>{
+  assert.match(ux,/@font-face\{font-family:\"KG Second Chances Sketch\"/);
+  assert.match(ux,/url\(\"\.\/assets\/fonts\/KGSecondChancesSketch\.ttf\"\)/);
+  assert.match(ux,/font-display:swap/);
 });
 
-test('bundled font loader contains an embedded WOFF2 face',()=>{
-  const fontLoader=fs.readFileSync(path.join(__dirname,'../src/kg-second-chances-sketch-font.js'),'utf8');
-  assert.match(fontLoader,/@font-face/);
-  assert.match(fontLoader,/font-family:\s*['\"]KG Second Chances Sketch['\"]/);
-  assert.match(fontLoader,/data:font\/woff2;base64,/);
+test('chalkboard text uses the bundled family before fallbacks',()=>{
+  assert.match(ux,/\.presentation-chalkboard \.academy-board-object-text\{[^}]*font-family:\"KG Second Chances Sketch\"/);
 });
