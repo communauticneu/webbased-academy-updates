0c3c234d8fc8743fd02fe068bb08217c21069df8
communauticneu <office@communautic.com>
2026-08-30 17:09:04 +0200

    test: lock specified chalk fonts by text kind

--- test/v16.25-chalk-font-single-source.test.js
+++ test/v16.25-chalk-font-single-source.test.js
@@ -5,8 +5,8 @@ const path=require('node:path');
 
 const ux=fs.readFileSync(path.join(__dirname,'../src/presentation-text-direct-ux.js'),'utf8');
 
-test('chalkboard heading normal and small use only Academy KG Sketch',()=>{
+test('chalkboard heading uses Academy KG Sketch and normal uses DJB Chalk It Up only',()=>{
   assert.match(ux,/academy-text-heading[^`]*Academy KG Sketch/s);
-  assert.match(ux,/academy-text-normal[^`]*Academy KG Sketch/s);
-  assert.doesNotMatch(ux,/DJB Chalk It Up|Segoe Print|Comic Sans MS/);
+  assert.match(ux,/academy-text-normal[^`]*DJB Chalk It Up/s);
+  assert.doesNotMatch(ux,/Segoe Print|Comic Sans MS/);
 });
