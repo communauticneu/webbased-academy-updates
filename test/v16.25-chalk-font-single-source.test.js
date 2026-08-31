d9eaf0cd2284dbb1230ff9ece2cc999db31eb5ef
communauticneu <office@communautic.com>
2026-08-31 13:18:25 +0200

    test: catch competing legacy chalk font assignment

--- test/v16.25-chalk-font-single-source.test.js
+++ test/v16.25-chalk-font-single-source.test.js
@@ -3,6 +3,7 @@ const assert=require('node:assert/strict');
 const fs=require('node:fs');
 const path=require('node:path');
 const ux=fs.readFileSync(path.join(__dirname,'../src/presentation-text-direct-ux.js'),'utf8');
+const editor=fs.readFileSync(path.join(__dirname,'../src/presentation-object-editor.js'),'utf8');
 const fonts=fs.readFileSync(path.join(__dirname,'../src/academy-fonts.css'),'utf8');
 
 test('Tafel heading is KG and Normal plus Klein are DJB Chalk It Up',()=>{
@@ -12,4 +13,8 @@ test('Tafel heading is KG and Normal plus Klein are DJB Chalk It Up',()=>{
  assert.match(ux,/academy-text-normal[\s\S]*Academy DJB Chalk/);
  assert.match(ux,/academy-text-small[\s\S]*Academy DJB Chalk/);
  assert.doesNotMatch(ux,/Segoe Print|Comic Sans MS/);
+});
+
+test('legacy object editor does not assign a competing chalk font family',()=>{
+ assert.doesNotMatch(editor,/\.presentation-chalkboard \.academy-board-object-text\{[^}]*font-family:/);
 });
