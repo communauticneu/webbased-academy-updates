b98b34d4af8f0054b77959f00b4a0d8c8bb6f57c
communauticneu <office@communautic.com>
2026-08-31 18:31:25 +0200

    test: require object editor to own chalk fonts

--- test/debug-font-single-source.test.js
+++ test/debug-font-single-source.test.js
@@ -4,11 +4,14 @@ const fs=require('node:fs');
 const path=require('node:path');
 
 const ux=fs.readFileSync(path.join(__dirname,'../src/presentation-text-direct-ux.js'),'utf8');
+const editor=fs.readFileSync(path.join(__dirname,'../src/presentation-object-editor.js'),'utf8');
 
-test('chalk text font ownership stays in CSS, not interaction inline styles',()=>{
-  assert.match(ux,/academy-text-heading[^`]*font-family:\"Academy KG Sketch\"!important/);
-  assert.match(ux,/academy-text-normal[^`]*font-family:\"Academy DJB Chalk\"!important/);
-  assert.match(ux,/academy-text-small[^`]*font-family:\"Academy DJB Chalk\"!important/);
+test('chalk text font ownership stays in the object editor CSS only',()=>{
+  assert.match(editor,/presentation-chalkboard[^']*academy-text-heading[^']*font-family:\"Academy KG Sketch\"!important/);
+  assert.match(editor,/presentation-chalkboard[^']*academy-text-normal[^']*font-family:\"Academy DJB Chalk\"!important/);
+  assert.match(editor,/presentation-chalkboard[^']*academy-text-small[^']*font-family:\"Academy DJB Chalk\"!important/);
+  assert.doesNotMatch(ux,/font-family:\"Academy KG Sketch\"!important/);
+  assert.doesNotMatch(ux,/font-family:\"Academy DJB Chalk\"!important/);
   assert.doesNotMatch(ux,/setProperty\(['\"]font-family['\"]/);
   assert.doesNotMatch(ux,/setProperty\(['\"]font-size['\"]/);
 });
